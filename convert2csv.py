596931077ce5b5a721a4a55ff6d7991c003a944b
Kevin Bimonte <kcbimonte@wpi.edu>
2019-10-01 23:53:01 -0400

    Initial Commit

--- convert2csv.py
+++ convert2csv.py
@@ -1,23 +1,25 @@
-#convert to csv
+# convert to csv
 import sys
 import json
 import csv
 
-ifilename = sys.argv[1]
+ifilename = "Yelp Data Files/JSON Files/review.json"
 try:
- ofilename = sys.argv[2]
+    ofilename = sys.argv[2]
 except:
- ofilename = ifilename + ".csv"
+    ofilename = ifilename + ".csv"
 
 # LOAD DATA
-json_lines = [json.loads( l.strip() ) for l in open(ifilename).readlines() ]
+json_lines = [json.loads(l.strip()) for l in open(ifilename, encoding="utf8").readlines()]
 OUT_FILE = open(ofilename, "w")
 root = csv.writer(OUT_FILE)
-root.writerow(["business_id","name","address","hours","is_open","categories","city","state","review_count","stars","postal_code","longitude","latitude","attributes"])
+root.writerow(["review_id", "user_id", "business_id", "stars", "date", "text", "useful", "funny", "cool"])
 json_no = 0
 for l in json_lines:
- root.writerow([l["business_id"], l["name"],l["address"],l["hours"],l["is_open"],l["categories"],l["city"],l["state"],l["review_count"],l["stars"],l["postal_code"],l["longitude"],l["latitude"],l["attributes"]])
- json_no += 1
+    root.writerow(
+        [l["review_id"], l["user_id"], l["business_id"], l["stars"], l["date"], l["text"], l["useful"], l["funny"],
+         l["cool"]])
+    json_no += 1
 
 print('Finished {0} lines'.format(json_no))
 OUT_FILE.close()
