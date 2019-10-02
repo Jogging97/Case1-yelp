ab31741d88af9638b2bfe9de323cfb1dd0fab688
Kevin Bimonte <kcbimonte@wpi.edu>
2019-10-02 00:57:51 -0400

    Horrible work in progress

---
+++ Reviews/negativeReviews.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+import Reviews.reviewHelpers as rh
+
+businesses = pd.read_csv("Yelp Data Files/CSV Files/business.csv")
+reviews = pd.read_csv()
+# users = pd.read_csv()
+photos = pd.read_csv("Yelp Data Files/CSV Files/photo.csv")
+
+negativeReviews = reviews[reviews["stars"] < 3]
+positiveReviews = reviews[reviews["stars"] >= 3]
+
+cntNegReviews = len(negativeReviews)
+cntPosReviews = len(positiveReviews)
+
+num_business_analysis = 1 # basically this will tell how much computing and diverse our analysis will be
+business_ids=bottom_business_data.sort_values("rated")[::-1][:num_business_analysis].business_id.values
+business_names = bottom_business_data.sort_values("rated")[::-1][:num_business_analysis]["Business name"].values
+# get all the reviews and analyse them
+#business_names
+for i, business_id in enumerate(business_ids):
+    # now extract reviews from reviews data
+    print("Analysing business: ", business_names[i])
+    reviews = reviews.loc[reviews['business_id'] == business_id].text.values
+    most_used_text = rh.count_ngrams(reviews, max_length=3)
+    rh.print_most_frequent(most_used_text, num=10)
---
+++ Reviews/reviewHelpers.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+
+import collections
+import re, string
+import sys
+import time
+from nltk.corpus import stopwords
+
+from subprocess import check_output
+
+
+# these are helper functions
+# directly copied from https://gist.github.com/benhoyt/dfafeab26d7c02a52ed17b6229f0cb52
+
+def tokenize(s):
+    """Convert string to lowercase and split into words (ignoring
+    punctuation), returning list of words.
+    """
+    word_list = re.findall(r'\w+', s.lower())
+    filtered_words = [word for word in word_list if word not in stopwords.words('english')]
+    return filtered_words
+
+
+def count_ngrams(lines, min_length=2, max_length=4):
+    """Iterate through given lines iterator (file object or list of
+    lines) and return n-gram frequencies. The return value is a dict
+    mapping the length of the n-gram to a collections.Counter
+    object of n-gram tuple and number of times that n-gram occurred.
+    Returned dict includes n-grams of length min_length to max_length.
+    """
+    lengths = range(min_length, max_length + 1)
+    ngrams = {length: collections.Counter() for length in lengths}
+    queue = collections.deque(maxlen=max_length)
+
+    # Helper function to add n-grams at start of current queue to dict
+    def add_queue():
+        current = tuple(queue)
+        for length in lengths:
+            if len(current) >= length:
+                ngrams[length][current[:length]] += 1
+
+    # Loop through all lines and words and add n-grams to dict
+    for line in lines:
+        for word in tokenize(line):
+            queue.append(word)
+            if len(queue) >= max_length:
+                add_queue()
+
+    # Make sure we get the n-grams at the tail end of the queue
+    while len(queue) > min_length:
+        queue.popleft()
+        add_queue()
+
+    return ngrams
+
+
+def print_most_frequent(ngrams, num=10):
+    """Print num most common n-grams of each length in n-grams dict."""
+    for n in sorted(ngrams):
+        print('----- {} most common {}-word phrase -----'.format(num, n))
+        for gram, count in ngrams[n].most_common(num):
+            print('{0}: {1}'.format(' '.join(gram), count))
+        print('')
