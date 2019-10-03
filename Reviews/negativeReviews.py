ad37d97743c31d7a90184032aff3a78713d6abed
Kevin Bimonte <kcbimonte@wpi.edu>
2019-10-03 09:10:58 -0400

    Putting things into functions

--- Reviews/negativeReviews.py
+++ Reviews/negativeReviews.py
@@ -3,46 +3,48 @@ import pandas as pd
 
 import Reviews.reviewHelpers as rh
 
-businesses = pd.read_csv("../Yelp Data Files/CSV Files/business.csv")
-reviews = pd.read_csv("../Yelp Data Files/CSV Files/review.csv")
-users = pd.read_csv("../Yelp Data Files/CSV Files/user.csv")
-photos = pd.read_csv("../Yelp Data Files/CSV Files/photo.csv")
+def main():
+    # businesses = pd.read_csv("../Yelp Data Files/CSV Files/business.csv")
+    reviews = pd.read_csv("../Yelp Data Files/CSV Files/review.csv")
+    # users = pd.read_csv("../Yelp Data Files/CSV Files/user.csv")
+    # photos = pd.read_csv("../Yelp Data Files/CSV Files/photo.csv")
 
-negativeReviews = reviews[reviews["stars"] < 2]
-positiveReviews = reviews[reviews["stars"] >= 3]
+    negativeReviews = reviews[reviews["stars"] < 2]
+    positiveReviews = reviews[reviews["stars"] > 3]
 
-cntNegReviews = len(negativeReviews)
-cntPosReviews = len(positiveReviews)
+    cntNegReviews = len(negativeReviews)
+    cntPosReviews = len(positiveReviews)
 
-print("Count of Negative Views: ", cntNegReviews)
-print("Count of Positive Views: ", cntPosReviews)
+    print("Count of Negative Views: ", cntNegReviews)
+    print("Count of Positive Views: ", cntPosReviews)
 
-neg_reviews_dict = {}
+    wordmap(negativeReviews)
 
-for bID in negativeReviews["business_id"].values:
-    try:
-        neg_reviews_dict[bID] = neg_reviews_dict[bID] + 1
-    except:
-        neg_reviews_dict[bID] = 1
 
-negBus = pd.DataFrame.from_dict(data=neg_reviews_dict, orient="index")
+def getReviewType(userReview):
+    print("Hi")
 
-negBus.reset_index(inplace=True)
-negBus.columns = ['business_id', 'rated']
 
-print(negBus.head(15))
+def promptUser():
+    userReview = input("Please enter a review: ")
 
-negBus = negBus[negBus['rated'] > 200]
 
-print(negBus.head(15))
-# num_business_analysis = 1 # basically this will tell how much computing and diverse our analysis will be
-# business_ids=bottom_business_data.sort_values("rated")[::-1][:num_business_analysis].business_id.values
-# business_names = bottom_business_data.sort_values("rated")[::-1][:num_business_analysis]["Business name"].values
-# # get all the reviews and analyse them
-# #business_names
-# for i, business_id in enumerate(business_ids):
-#     # now extract reviews from reviews data
-#     print("Analysing business: ", business_names[i])
-#     reviews = reviews.loc[reviews['business_id'] == business_id].text.values
-#     most_used_text = rh.count_ngrams(reviews, max_length=3)
-#     rh.print_most_frequent(most_used_text, num=10)
+def wordmap(reviewDF):
+    reviews_dict = {}
+
+    for bID in reviewDF["business_id"].values:
+        try:
+            reviews_dict[bID] = reviews_dict[bID] + 1
+        except:
+            reviews_dict[bID] = 1
+
+    businesses = pd.DataFrame.from_dict(data=reviews_dict, orient="index")
+
+    businesses.reset_index(inplace=True)
+    businesses.columns = ['business_id', 'rated']
+
+    print(businesses.head(15))
+
+    businesses = businesses[businesses['rated'] > 200]
+
+    print(businesses.head(15))
