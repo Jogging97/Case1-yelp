38fe4cd329a85a3857936134f05f633e5adab8a0
Kevin Bimonte <kcbimonte@wpi.edu>
2019-10-02 20:03:12 -0400

    Working on negative reviews

--- Reviews/negativeReviews.py
+++ Reviews/negativeReviews.py
@@ -8,7 +8,7 @@ reviews = pd.read_csv("../Yelp Data Files/CSV Files/review.csv")
 users = pd.read_csv("../Yelp Data Files/CSV Files/user.csv")
 photos = pd.read_csv("../Yelp Data Files/CSV Files/photo.csv")
 
-negativeReviews = reviews[reviews["stars"] < 3]
+negativeReviews = reviews[reviews["stars"] < 2]
 positiveReviews = reviews[reviews["stars"] >= 3]
 
 cntNegReviews = len(negativeReviews)
@@ -17,6 +17,24 @@ cntPosReviews = len(positiveReviews)
 print("Count of Negative Views: ", cntNegReviews)
 print("Count of Positive Views: ", cntPosReviews)
 
+neg_reviews_dict = {}
+
+for bID in negativeReviews["business_id"].values:
+    try:
+        neg_reviews_dict[bID] = neg_reviews_dict[bID] + 1
+    except:
+        neg_reviews_dict[bID] = 1
+
+negBus = pd.DataFrame.from_dict(data=neg_reviews_dict, orient="index")
+
+negBus.reset_index(inplace=True)
+negBus.columns = ['business_id', 'rated']
+
+print(negBus.head(15))
+
+negBus = negBus[negBus['rated'] > 200]
+
+print(negBus.head(15))
 # num_business_analysis = 1 # basically this will tell how much computing and diverse our analysis will be
 # business_ids=bottom_business_data.sort_values("rated")[::-1][:num_business_analysis].business_id.values
 # business_names = bottom_business_data.sort_values("rated")[::-1][:num_business_analysis]["Business name"].values
